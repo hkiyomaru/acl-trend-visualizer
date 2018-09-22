e9d22e9356412f34cacd59a3ace6932626f57cf6
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2018-09-22 21:29:03 +0900

    update the visualizer

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,6 +1,4 @@
 """Visualizer for the results."""
-import os
-
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas
@@ -13,7 +11,7 @@ class Visualizer(object):
         self.results = self.preprocess(results)
 
     def pointplot(self, out):
-        plt.figure(num=None, figsize=(9, 6))
+        plt.figure(num=None, figsize=(20, 10))
 
         g = self.results.groupby("year")[self.words].sum()
         g.reset_index(inplace=True)
@@ -21,8 +19,9 @@ class Visualizer(object):
         df = g[["year"] + self.words]
         df = df.melt("year", var_name="cols", value_name="counts")
         ax = sns.pointplot(x="year", y="counts", hue="cols", data=df, palette="bright")
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
         fig = ax.get_figure()
-        fig.savefig(out)
+        fig.savefig(out, bbox_inches='tight')
 
     def preprocess(self, results):
         results = pandas.DataFrame(results)
