741f1505316230a78abc5a87e9160c3b0945b096
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2018-09-22 17:06:51 +0900

    tweak just a hair

--- src/crawler.py
+++ src/crawler.py
@@ -1,5 +1,4 @@
 """Crawler for ACL Anthology."""
-import json
 import multiprocessing
 import os
 import requests
@@ -16,9 +15,9 @@ from io import StringIO
 URL_FORMAT = "http://aclweb.org/anthology/P{year}-{ptype}{pid:03d}"
 
 
-class Investigator(object):
+class Crawler(object):
 
-    def __init__(self, years, ptypes, max_pid=300):
+    def __init__(self, years, ptypes, max_pid=10):
         """
 
         :param years: a list of strings which indicate years to gather.
@@ -27,27 +26,25 @@ class Investigator(object):
 
         """
         assert len(set(ptypes) - set("sl")) == 0, "Invalid ptypes were specified: %s" % ptypes
-        self.params = [{'year': year, 'ptype': ptype, 'pid': pid}
+        self.params = [{"year": year, "ptype": ptype, "pid": pid}
                        for year in years.split(",") for ptype in ptypes for pid in range(1, max_pid)]
 
-    def search(self, words, out, tmp_dir="tmp", jobs=1):
+    def search(self, words, tmp_dir="tmp", jobs=1):
         os.makedirs(tmp_dir, exist_ok=True)
 
         params = self._make_chunks(self.params, words, tmp_dir, jobs)
         with multiprocessing.Pool(processes=jobs) as pool:
             results = pool.starmap(self._search, params)
 
-        results = [item for subresults in results for item in subresults]
-        with open(out, "wt") as f:
-            json.dump(results, f)
+        return [item for subresults in results for item in subresults]
 
     def _search(self, params, words, tmp_dir):
         _results = []
         for param in params:
             url = URL_FORMAT.format(
-                year=param['year'],
-                ptype=self._make_ptype(param['ptype']),  # "l" -> 1, "s" -> 2
-                pid=param['pid'])
+                year=param["year"],
+                ptype=self._make_ptype(param["ptype"]),  # "l" -> 1, "s" -> 2
+                pid=param["pid"])
             basename = os.path.basename(url)
             path_paper = os.path.join(tmp_dir, basename)
             ok = self._save_paper(url, path_paper)
@@ -55,10 +52,11 @@ class Investigator(object):
                 continue
             else:
                 _results.append({
-                    'year': param['year'],
-                    'ptype': param['ptype'],
-                    'pid': param['pid'],
-                    'words': self._check_words(words, path_paper)})
+                    "url": url,
+                    "year": param["year"],
+                    "ptype": param["ptype"],
+                    "pid": param["pid"],
+                    "words": self._check_words(words, path_paper)})
                 self._delete_paper(path_paper)
         return _results
 
@@ -77,7 +75,11 @@ class Investigator(object):
                     interpreter.process_page(page)
                 return self._clean_text(outfp.getvalue())
 
-        text = convert_pdf_to_text(path)
+        try:
+            text = convert_pdf_to_text(path)
+        except Exception as e:
+            print(e, file=sys.stderr)
+            text = ""
         word_list = set(text.split())
         return [word for word in words if word in word_list]
 
@@ -94,7 +96,7 @@ class Investigator(object):
             print(e, file=sys.stderr)
             return 1
 
-        if r.url == 'https://www.aclweb.org/404.shtml':
+        if r.url == "https://www.aclweb.org/404.shtml":
             print("Status Code: 404", file=sys.stderr)
             return 1
         elif r.status_code == 200:
--- src/run_crawler.py
+++ src/run_crawler.py
@@ -1,18 +1,15 @@
 """Main script to run a crawler."""
 import argparse
 
-from investigator import Investigator
-
-
-def load_word_list(path):
-    with open(path) as f:
-        return [line.strip() for line in f if line.strip() != ""]
+from utils import load_word_list
+from utils import save_results
+from crawler import Crawler
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('WORDLIST', help='path to a word list file')
-    parser.add_argument('-y', '--year', default="16,17,18",
+    parser.add_argument('-y', '--year', default="13,14,15,16,17,18",
                         help='years of conferences (e.g., input "--year 16,17,18")')
     parser.add_argument('-t', '--type', default="ls", choices=["l", "s", "ls"],
                         help='submission types ("l" (long), "s" (short), or "ls")')
@@ -24,9 +21,10 @@ def main():
                         help='path to the output file')
     args = parser.parse_args()
 
-    investigator = Investigator(args.year, args.type)
     word_list = load_word_list(args.WORDLIST)
-    investigator.search(word_list, args.output, tmp_dir=args.tmp_dir, jobs=args.jobs)
+    investigator = Crawler(args.year, args.type)
+    results = investigator.search(word_list, tmp_dir=args.tmp_dir, jobs=args.jobs)
+    save_results(results, args.output)
 
 
 if __name__ == '__main__':
