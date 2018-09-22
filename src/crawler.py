a95056b5987c4339eb2bb5f6d4f4f54ad4112b85
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2018-09-22 18:18:23 +0900

    update to stip crawling after detecting 404

--- src/crawler.py
+++ src/crawler.py
@@ -17,17 +17,16 @@ URL_FORMAT = "http://aclweb.org/anthology/P{year}-{ptype}{pid:03d}"
 
 class Crawler(object):
 
-    def __init__(self, years, ptypes, max_pid=300):
+    def __init__(self, years, ptypes):
         """
 
         :param years: a list of strings which indicate years to gather.
         :param ptypes: a string which indicates submission type ("l" (long), "s" (short), or "lt").
-        :param max_pid: maximum number of accepted papers for a conference.
 
         """
         assert len(set(ptypes) - set("sl")) == 0, "Invalid ptypes were specified: %s" % ptypes
         self.params = [{"year": year, "ptype": ptype, "pid": pid}
-                       for year in years.split(",") for ptype in ptypes for pid in range(1, max_pid)]
+                       for year in years.split(",") for ptype in ptypes for pid in range(1, 1000)]
 
     def search(self, words, tmp_dir="tmp", jobs=1):
         os.makedirs(tmp_dir, exist_ok=True)
@@ -40,7 +39,8 @@ class Crawler(object):
 
     def _search(self, params, words, tmp_dir):
         _results = []
-        for param in params:
+        while params:
+            param = params.pop(0)
             url = URL_FORMAT.format(
                 year=param["year"],
                 ptype=self._make_ptype(param["ptype"]),  # "l" -> 1, "s" -> 2
@@ -48,9 +48,7 @@ class Crawler(object):
             basename = os.path.basename(url)
             path_paper = os.path.join(tmp_dir, basename)
             ok = self._save_paper(url, path_paper)
-            if ok > 0:
-                continue
-            else:
+            if ok == 0:  # success
                 _results.append({
                     "url": url,
                     "year": param["year"],
@@ -58,6 +56,19 @@ class Crawler(object):
                     "pid": param["pid"],
                     "words": self._check_words(words, path_paper)})
                 self._delete_paper(path_paper)
+            elif ok == 1:  # 404
+                while True:  # delete queues which has greater pids
+                    next_param = params[0]
+                    if next_param["year"] == param["year"] \
+                            and next_param["ptype"] == param["ptype"] \
+                            and next_param["pid"] > param["pid"]:
+                        params.pop(0)
+                        if len(params) == 0:
+                            return _results
+                    else:
+                        break
+            else:  # something wrong
+                continue
         return _results
 
     def _check_words(self, words, path):
@@ -98,7 +109,7 @@ class Crawler(object):
             r = requests.get(url)
         except requests.RequestException as e:
             print(e, file=sys.stderr)
-            return 1
+            return 2
 
         if r.url == "https://www.aclweb.org/404.shtml":
             print("Status Code: 404", file=sys.stderr)
@@ -109,7 +120,7 @@ class Crawler(object):
             return 0
         else:
             print("Status Code:", r.status_code, file=sys.stderr)
-            return 1
+            return 2
 
     @staticmethod
     def _delete_paper(path):
@@ -128,4 +139,5 @@ class Crawler(object):
         if n < 1:
             print("Invalid parallelism.")
             sys.exit(1)
-        return [(params[i::n], words, tmp_dir) for i in range(n)]
+        n_chunk = len(params) // n
+        return [(params[i:n_chunk+i], words, tmp_dir) for i in range(0, len(params), n_chunk)]
